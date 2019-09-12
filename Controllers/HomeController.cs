e9191938593b541de652904e402c491473a804a6
Евгений Попов <normund@normund.com.ua>
2019-09-12 12:03:26 +0300

    AddDatabase

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using smart_garden.Data;
 
 namespace smart_garden.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            return View(_db.BlogEntries);
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,13 +6,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using smart_garden.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace smart_garden
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(
+                 Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
